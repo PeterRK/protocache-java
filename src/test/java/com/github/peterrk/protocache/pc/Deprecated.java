d654c954ca77aa689c6575c81060f08acc0aa499
Ruan Kunliang <PeterRK@126.com>
2025-04-11 01:04:09 +0800

    update testcase

---
+++ src/test/java/com/github/peterrk/protocache/pc/Deprecated.java
@@ -0,0 +1,14 @@
+package com.github.peterrk.protocache.pc;
+
+public final class Deprecated extends com.github.peterrk.protocache.Message {
+	public final static class Valid extends com.github.peterrk.protocache.Message {
+
+		public static final int FIELD_val = 0;
+
+		public Valid(){}
+		public Valid(byte[] data){ this(data, 0); }
+		public Valid(byte[] data, int offset){ super(data, offset); }
+
+		public int getVal() { return getInt32(FIELD_val); }
+	}
+}
